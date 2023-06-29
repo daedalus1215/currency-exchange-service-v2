bb2ff4d0ec746a9df5e827fa94b40e58e8ee3a59
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-28 21:44:20 -0400

    fill in controller and entity

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,2 +1,21 @@
-package com.in28minutes.microservices.currencyexchangeservice;public class CurrencyExchange {
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CurrencyExchange implements Serializable {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private String environment;
 }
