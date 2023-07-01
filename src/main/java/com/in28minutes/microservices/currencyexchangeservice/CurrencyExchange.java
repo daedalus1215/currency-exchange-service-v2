b540c7da0d9eb6099406446bbeab575d091848bf
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-30 22:27:53 -0400

    Add Entity to currencyExchange

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,5 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +15,16 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class CurrencyExchange implements Serializable {
+    @Id
     private Long id;
+    @Column(name="currency_from") // Need to give this a column name, since 'from' is a SQL keyword.
     private String from;
+    @Column(name="currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
+
+
 }
