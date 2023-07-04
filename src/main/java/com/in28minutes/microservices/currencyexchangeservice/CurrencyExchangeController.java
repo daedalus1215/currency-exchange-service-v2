58f7f7568cade89de6a929d991e85bd90c7c5a82
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-07-04 08:45:41 -0400

    Add the logic to get CurrencyExchange in Controller

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -6,16 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+
     private
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50), environment.getProperty("local.server.port"));
+        CurrencyExchange byFromAndTo = currencyExchangeRepository.findByFromAndTo(from, to);
+        if (byFromAndTo == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
+
+        return byFromAndTo;
     }
 }
