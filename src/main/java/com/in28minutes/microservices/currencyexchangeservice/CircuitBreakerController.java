134c116a09fb3ec8e431a9d892233d2675af21a5
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-07-13 22:12:00 -0400

    Adding a circuit breaker

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +14,8 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "default", fallbackMethod = "  hardcodedResponse")
+//    @Retry(name = "default", fallbackMethod = "  hardcodedResponse")
+    @CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
     public String sampleApi() throws Exception {
         logger.info("Sample Api call received");
         final ResponseEntity<String> forEntity = new RestTemplate()
