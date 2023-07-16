762a6b391e565c5b6feee50952d9d02a6efe4766
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-07-16 11:37:27 -0400

    1.0 = zipkin compliant

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # currency-exchange-service-v2
 * Add VM Argument, if we want to change what port we are starting up on: `-Dserver.port=8001`
+V1.0:
+* Needs Zipkin running locally, if we want to capitalize on the distributed tracing.
--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -16,6 +20,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange byFromAndTo = currencyExchangeRepository.findByFromAndTo(from, to);
         if (byFromAndTo == null) {
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
